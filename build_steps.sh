fadb252fa4b2191695262be8f7d186c6c9d4da72
harshapte26 <hsapte99@gmail.com>
2023-11-10 22:36:31 -0500

    variable multiplication working

---
+++ build_steps.sh
@@ -0,0 +1,4 @@
+cmake -S src -B build
+cmake --build build
+cd build/
+./test0_dd
--- src/test_dd.cc
+++ src/test_dd.cc
@@ -10,13 +10,13 @@
 
 class Constant;
 class Op;
-class Equation;
+class Variable;
 
 class Visitor {
 public: 
     virtual void Visit(Constant &e) = 0;
     virtual void Visit(Op &e) = 0;
-    virtual void Visit(Equation &e) = 0;
+    virtual void Visit(Variable &e) = 0;
 };
 
 class Expression
@@ -99,32 +99,56 @@ public:
     std::unique_ptr<Expression> r_;
 };
 
-class Equation : public Expression {
+class Variable final: public Expression{
+
 public:
-  Equation(std::unique_ptr<Expression> expr) : expr_(std::move(expr)) {
-    RegisterLoader("Equation", Load);
-  }
 
-  void Accept(Visitor &visitor) override {
-    visitor.Visit(*this); 
-  }
+    Variable(char v) : v_(v) {
+        RegisterLoader("Variable", Load);
+    }
+    
+    void Accept(Visitor &visitor) override {
+        visitor.Visit(*this);
+    }
 
-  static std::unique_ptr<Expression> Load(std::istream& s) {
-    return std::make_unique<Equation>(Expression::Load(s));
-  }
+    static std::unique_ptr<Expression> Load(std::istream &s)
+    {
+        char v;
+        s >> v;
+        return std::make_unique<Variable>(v);
+    }
 
-    char op_ = '='; 
+    char v_;
 
-// private:
-  std::unique_ptr<Expression> expr_;
+
 };
+
+// class Equation : public Expression {
+// public:
+//   Equation(std::unique_ptr<Expression> expr) : expr_(std::move(expr)) {
+//     RegisterLoader("Equation", Load);
+//   }
+
+//   void Accept(Visitor &visitor) override {
+//     visitor.Visit(*this); 
+//   }
+
+//   static std::unique_ptr<Expression> Load(std::istream& s) {
+//     return std::make_unique<Equation>(Expression::Load(s));
+//   }
+
+//     char op_ = '='; 
+
+// // private:
+//   std::unique_ptr<Expression> expr_;
+// };
 
 std::unique_ptr<Expression> parse(std::string t)
 {   
     
-    if (t == "Equation") {
-        return std::make_unique<Equation>(parse(t.substr(1))); 
-        }
+    // if (t == "Equation") {
+    //     return std::make_unique<Equation>(parse(t.substr(1))); 
+    //     }
     
     // TODO :: logic to handle variable if ()
 
@@ -137,13 +161,22 @@ std::unique_ptr<Expression> parse(std::string t)
     for (int i = t.size(); i >= 0; i--)
         if (t[i] == '^')
             return std::make_unique<Op>(t[i], parse(t.substr(0, i)), parse(t.substr(i + 1)));
+    
+    // if a string is variable, check for that
+    if (t.size() == 1)
+            return std::make_unique<Variable>(t[0]);    
     return std::make_unique<Constant>(std::stoi(t));
+    // return std::make_unique<Constant>(t);
 }
 
 class ComputeVisitor: public Visitor {
 public:
     void Visit(Constant &e) {
         result_ = e.x_;
+    }
+
+    void Visit(Variable &e) {
+        result_ = e.v_;
     }
 
     void Visit(Op &e) {
@@ -174,19 +207,71 @@ public:
         }
     }
 
-    void Visit(Equation &i) {
-        i.expr_->Accept(*this);
-        result_ = result_; 
-        }
+    // void Visit(Equation &i) {
+    //     i.expr_->Accept(*this);
+    //     result_ = result_; 
+    //     }
 
 //private:
     double result_;
+};
+
+class SimplifyVisitor: public Visitor {
+public:
+    void Visit(Constant &e) {
+        result_ = e.x_;
+    }
+
+    void Visit(Variable &e) {
+        result_ = e.v_;
+    }
+
+    void Visit(Op &e) {
+        e.l_->Accept(*this);
+        char l = result_[0];
+        std::cout<<"this is l --> "<<l<<"\n";
+        e.r_->Accept(*this);
+        double r = result_[0]-'0';
+        std::cout<<"this is r --> "<<r<<"\n";
+        std::cout<<"this is op --> "<<e.op_<<"\n";
+        switch (e.op_)
+        {
+        case '+':
+            result_ = l + r;
+            break;
+        case '-':
+            result_ = l - r;
+            break;
+        case '*':
+            result_ = std::string(r, l);
+            std::cout<<"this is result --> "<<result_<<"\n";
+            break;
+        case '/':
+            result_ = l / r;
+            break;
+        default:
+            std::cout << "invalid op " << e.op_;
+            abort();
+        }
+    }
+
+    // void Visit(Equation &i) {
+    //     i.expr_->Accept(*this);
+    //     result_ = result_; 
+    //     }
+
+//private:
+    std::string result_;
 };
 
 class PrettyPrintVisitor: public Visitor {
 public:
     void Visit(Constant &e) {
         result_ = (std::stringstream() << "(" << e.x_ << ")").str();
+    }
+
+    void Visit(Variable &e) {
+        result_ = (std::stringstream() << "(" << e.v_ << ")").str();
     }
 
     void Visit(Op &e) {
@@ -199,13 +284,15 @@ public:
         result_ = result.str();
     }
 
-    void Visit(Equation &i) {
-        i.expr_->Accept(*this);
-        result_ = result_ + "= " + result_;
-}
+//     void Visit(Equation &i) {
+//         i.expr_->Accept(*this);
+//         result_ = result_ + "= " + result_;
+// }
 
     std::string result_;
 };
+
+
 
 class SaveVisitor: public Visitor {
 public:
@@ -227,26 +314,26 @@ public:
 };
 
 TEST(Expressions, Test2) {
-    std::string s = "1+2*3-4";
-    auto e = parse(s);
+    // std::string s = "1+2*3-4";
+    // auto e = parse(s);
 
-    auto v = ComputeVisitor();
-    e->Accept(v);
-    std::cout << v.result_ << std::endl;
+    // auto v = ComputeVisitor();
+    // e->Accept(v);
+    // // std::cout << v.result_ << std::endl;
 
-    auto v2 = PrettyPrintVisitor();
-    e->Accept(v2);
-    std::cout << v2.result_ << std::endl;
+    // auto v2 = PrettyPrintVisitor();
+    // e->Accept(v2);
+    // std::cout << v2.result_ << std::endl;
 
-    auto g = parse("a*1");
-    auto v3 = ComputeVisitor();
+    auto g = parse("j-3");
+    auto v3 = SimplifyVisitor();
     g->Accept(v3); // Prints 2
 
     auto v4 = PrettyPrintVisitor();
     g->Accept(v4); // Prints (2*1)=2
 
     std::cout << v3.result_ << std::endl;
-    std::cout << v4.result_ << std::endl;
+    // std::cout << v4.result_ << std::endl;
 
     // std::cout << e->compute() << std::endl;
     // std::cout << e->PrettyPrint2() << std::endl;
